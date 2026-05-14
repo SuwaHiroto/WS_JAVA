f6587741c8fdc4736997dfd2b7d5533c1eefa5ba
Derian Gonzales <190986435+SuwaHiroto@users.noreply.github.com>
2026-05-13 22:05:24 -0500

    Add files via upload

---
+++ HolaMundoRest/src/main/java/com/example/holamundorest/HolaController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.holamundorest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/api/hola")
+    public Map<String, String> saludar() {
+        Map<String, String> respuesta = new HashMap<>();
+        respuesta.put("mensaje", "¡Hola Mundo desde Java!");
+        respuesta.put("estado", "activo");
+        return respuesta;
+    }
+}
